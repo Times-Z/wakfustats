3d022bd6ea61b4361425ea9e2137d3e68b2a3453
BenoitDeligny <delignyb.pro@gmail.com>
2025-05-14 22:56:27 +0200

    Add heal regex

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ To run: ./gradlew run
 TODO:
 - Opacity control
 - Handle reset screen in better way
-- Compute damages
 - Compute heals
 - Compute shields
--- src/main/java/com/wakfoverlay/exposition/RegexProvider.java
+++ src/main/java/com/wakfoverlay/exposition/RegexProvider.java
@@ -6,9 +6,9 @@ public class RegexProvider {
     private final Pattern CAST_SPELL_PATTERN = Pattern
             .compile(".*\\[Information\\s*\\(jeu\\)\\]\\s*(.*)\\s+lance\\s+le\\s+sort\\s+(.*)");
     private final Pattern DAMAGES_PATTERN = Pattern
-            .compile(".*\\[Information\\s*\\(jeu\\)\\]\\s*([^:]+):\\s*([+-]\\d+)\\s+PV\\s+\\((.*)\\)");
+            .compile(".*\\[Information\\s*\\(jeu\\)\\]\\s*([^:]+):\\s*([-]\\d+)\\s+PV\\s+\\((.*)\\)");
     private final Pattern HEALS_PATTERN = Pattern
-            .compile(".*\\[Information\\s*\\(jeu\\)\\]\\s*([^:]+):\\s*([+-]\\d+)\\s+PV\\s+\\((.*)\\)");
+            .compile(".*\\[Information\\s*\\(jeu\\)\\]\\s*([^:]+):\\s*([+]\\d+)\\s+PV\\s+\\((.*)\\)");
 
     public Pattern castSpellPattern() {
         return CAST_SPELL_PATTERN;
